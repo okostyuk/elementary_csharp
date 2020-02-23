ec8a63bcb6fbe32abfd33661eacb798a1b18537f
Oleg Kostiuk <kostyuk.oleg@gmail.com>
2020-02-23 20:52:27 +0200

    fix breaks

--- src/sql/breaks.sql
+++ src/sql/breaks.sql
@@ -1,14 +1,19 @@
-select * from 
-(
-    select D.LNAME, B.NAME,
-           DATEDIFF(
-                   MINUTE,
-                   DATE,
-                   (LEAD(DATE, 1, DATE) OVER (ORDER BY DATE))
-       ) as break
-    from VISITS V
-             join DOCTORS D on V.DOCTOR_ID = D.ID
-             join BRANCHES B on V.BRANCH_ID = B.ID
-    order by break              
-) as BREAKS
-where BREAKS.break > 30
+//перерывы в работе врача более чем 30 минут, выводятся по уменьшению длительности перерыва. 
+//Выводить нужно колонки 
+//имя врача, его отделение, дата и время начала перерыва, длительность и конец перева.
+
+select BRANCHES.NAME as "Branch name", 
+       DOCTORS.FNAME||' '||DOCTORS.LNAME as "Doctor name", DATEDIFF(MINUTE, DATE, NEXT_DATE) as break_time
+from (
+         select DOCTOR_ID,
+                BRANCH_ID,
+                DATE,
+                LEAD(DATE, 1, null) OVER (ORDER BY DOCTOR_ID, BRANCH_ID) as NEXT_DATE 
+         from visits
+         order by BRANCH_ID, DOCTOR_ID, DATE
+    ) as BREAKS
+join BRANCHES on BRANCHES.ID = BREAKS.BRANCH_ID
+join DOCTORS on DOCTORS.ID = BREAKS.DOCTOR_ID
+where NEXT_DATE is not null
+and DATEDIFF(MINUTE, DATE, NEXT_DATE) > 30
+order by DATEDIFF(MINUTE, DATE, NEXT_DATE) desc
--- src/sql/doctors_stats.sql
+++ src/sql/doctors_stats.sql
@@ -6,24 +6,24 @@
 
 select D.LNAME, Q1.TOTAL_UNIQUE_PATIENTS, Q2.AVG_VISITS_PER_MONTH
 from DOCTORS as D
-         left join (
-    select DOCTOR_ID, count(DOCTOR_ID) as TOTAL_UNIQUE_PATIENTS
-    from (
-             select DOCTOR_ID, count(*) from VISITS
-             group by DOCTOR_ID, PATIENT_ID
-         )
-    group by DOCTOR_ID
-) as Q1 on D.ID = Q1.DOCTOR_ID
-         left join (
-    select DOCTOR_ID, AVG(CNT) as AVG_VISITS_PER_MONTH
-    from (
-             select DOCTOR_ID, MONTH(DATE) as _MONTH, count(*) as CNT
-             from VISITS
-             group by DOCTOR_ID, _MONTH
-         )
-    group by DOCTOR_ID
-) as Q2 on D.ID = Q2.DOCTOR_ID
-         left join (
+left join (
+        select DOCTOR_ID, count(DOCTOR_ID) as TOTAL_UNIQUE_PATIENTS
+        from (
+                 select DOCTOR_ID, count(*) from VISITS
+                 group by DOCTOR_ID, PATIENT_ID
+             )
+        group by DOCTOR_ID
+    ) as Q1 on D.ID = Q1.DOCTOR_ID
+left join (
+        select DOCTOR_ID, AVG(CNT) as AVG_VISITS_PER_MONTH
+        from (
+                 select DOCTOR_ID, MONTH(DATE) as _MONTH, count(*) as CNT
+                 from VISITS
+                 group by DOCTOR_ID, _MONTH
+             )
+        group by DOCTOR_ID
+    ) as Q2 on D.ID = Q2.DOCTOR_ID
+left join (
     select Q3.DOCTOR_ID, max(Q3.DIAGNOSIS_ID) as MOST_FREQ_DIAG
     from (
              select DOCTOR_ID, DIAGNOSIS_ID, count(*) CNT from VISITS
